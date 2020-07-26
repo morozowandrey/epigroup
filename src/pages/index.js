01e30f3c7c63c40040fe39cd22960f20ea58e244
oslobird <andrey940@gmail.com>
2020-07-26 14:41:58 +0300

    recaptcha removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../layout'
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../layout";
 export default function Index() {
-  return (
-    <Layout>
-      <h1>Hi people</h1>
-      <p>
-        This is an example site integrating Netlify’s form handling with Gatsby. You can{' '}
-        <a href="https://github.com/sw-yx/gatsby-netlify-form-example-v2">view source here</a>.
-      </p>
-      <ul>
-        <li>
-          <Link to="/contact/">Basic contact form</Link>
-        </li>
-        <li>
-          <Link to="/file-upload/">Form with file upload</Link>
-        </li>
-        <li>
-          <Link to="/multipart-file-upload/">Form with multipart file upload</Link>
-        </li>
-        <li>
-          <Link to="/recaptcha/">Form with reCAPTCHA 2</Link>
-        </li>
-        <li>
-          You can also{' '}
-          <a href="https://github.com/snipcart/gatsby-netlify-serverless/blob/master/functions/fetchreviews.js#L23">
-            programmatically retrieve data
-          </a>
-          .
-        </li>
-      </ul>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<h1>Hi people</h1>
+			<p>
+				This is an example site integrating Netlify’s form handling with
+				Gatsby. You can{" "}
+				<a href="https://github.com/sw-yx/gatsby-netlify-form-example-v2">
+					view source here
+				</a>
+				.
+			</p>
+			<ul>
+				<li>
+					<Link to="/contact/">Basic contact form</Link>
+				</li>
+				<li>
+					<Link to="/file-upload/">Form with file upload</Link>
+				</li>
+				<li>
+					<Link to="/multipart-file-upload/">
+						Form with multipart file upload
+					</Link>
+				</li>
+				<li>
+					You can also{" "}
+					<a href="https://github.com/snipcart/gatsby-netlify-serverless/blob/master/functions/fetchreviews.js#L23">
+						programmatically retrieve data
+					</a>
+					.
+				</li>
+			</ul>
+		</Layout>
+	);
 }
