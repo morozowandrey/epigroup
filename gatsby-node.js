edb75bc1014f6824da86117a9cf6dbb5c05144a1
oslobird <andrey940@gmail.com>
2020-08-12 23:18:23 +0300

    loadable test

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+// 	if (stage === "build-html") {
+// 		actions.setWebpackConfig({
+// 			// module: {
+// 			// 	rules: [
+// 			// 		{
+// 			// 			test: /tiny-slider/,
+// 			// 			use: loaders.null(),
+// 			// 		},
+// 			// 	],
+// 			// },
+// 			resolve: {
+// 				modules: [path.resolve(__dirname, "src"), "node_modules"],
+// 			},
+// 		});
+// 	}
+// };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,18 @@ import Hero from "../components/hero";
 import About from "../components/about";
 import Contact from "../components/contact";
 import Results from "../components/results";
-import ActivitySlider from "../components/activity-slider";
-import TradeInvestments from "../components/trade-investments";
+// import ActivitySlider from "../components/activity-slider";
+// import LoadableActivitySlider from "../components/activity-slider";
+// import TradeInvestments from "../components/trade-investments";
+import loadable from "@loadable/component";
+// This dynamic import will not be processed server-side
+const Other = loadable(() => import("../components/activity-slider"), {
+	ssr: false,
+});
+const Other2 = loadable(() => import("../components/trade-investments"), {
+	ssr: false,
+});
+
 export default function Index() {
 	if (typeof window !== "undefined") {
 		// Make scroll behavior of internal links smooth
@@ -16,8 +26,12 @@ export default function Index() {
 		<Layout>
 			<Hero />
 			<About />
-			<ActivitySlider />
-			<TradeInvestments />
+			{/* <ActivitySlider /> */}
+			<Other />
+			<Other2 />
+
+			{/* <LoadableActivitySlider /> */}
+			{/* <TradeInvestments /> */}
 			<Results />
 			<Contact />
 		</Layout>
